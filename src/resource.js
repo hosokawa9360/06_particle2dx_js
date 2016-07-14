cb4eaa985f4e4e04985097df50cd716d2541b668
Junya Hosokawa <hosokawa@morijyobi.com>
2016-07-14 12:12:05 +0900

    背景とキャラクター、パーティクルを表示させる

--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,13 @@
 var res = {
     HealTexture_png : "res/Heal_texture.png",
     DotTexture_png : "res/Dot_texture.png",
+    chara_princessselect_10 : "res/char/chara_princessselect_10.png",
+    chara_princessselect_11 : "res/char/chara_princessselect_11.png",
+    chara_princessselect_12 : "res/char/chara_princessselect_12.png",
+    ss_BattleScene_bg1 : "res/field/ss_BattleScene_bg1.jpg",
+    ss_BattleScene_bg2 : "res/field/ss_BattleScene_bg2.jpg",
+    ss_BattleScene_bg3 : "res/field/ss_BattleScene_bg3.jpg",
+
 };
 
 var g_resources = [];
