b8e112f1314b061597474ed34644775baa508499
Junya Hosokawa <hosokawa@morijyobi.com>
2016-08-17 09:43:10 +0900

    resource.js edit

--- src/resource.js
+++ src/resource.js
@@ -1,12 +1,19 @@
 var res = {
     HealTexture_png : "res/Heal_texture.png",
     DotTexture_png : "res/Dot_texture.png",
+    particle_texture : "res/Dot_texture.png",
     chara_princessselect_10 : "res/char/chara_princessselect_10.png",
     chara_princessselect_11 : "res/char/chara_princessselect_11.png",
     chara_princessselect_12 : "res/char/chara_princessselect_12.png",
     ss_BattleScene_bg1 : "res/field/ss_BattleScene_bg1.jpg",
     ss_BattleScene_bg2 : "res/field/ss_BattleScene_bg2.jpg",
     ss_BattleScene_bg3 : "res/field/ss_BattleScene_bg3.jpg",
+    chara_enemy_1 : "res/char/chara_enemy_1.png",
+    chara_enemy_2 : "res/char/chara_enemy_2.png",
+    chara_enemy_3 : "res/char/chara_enemy_3.png",
+    chara_enemy_4 : "res/char/chara_enemy_4.png",
+    chara_enemy_5 : "res/char/chara_enemy_5.png",
+    chara_enemy_6 : "res/char/chara_enemy_6.png",
 
 };
 
